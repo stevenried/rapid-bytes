85c1252a42e6e116b3ab4f3407e079a80243799e
Steven Ried <s_ried@msn.com>
2022-02-13 22:40:06 -0500

    Add tab functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+// nav menu appearance
 const burger = document.getElementsByClassName('burger')[0];
 const menuLinks = document.getElementsByClassName('menu-links')[0];
 
 burger.addEventListener('click', () => {
   menuLinks.classList.toggle('active');
+});
+
+//tabs
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active');
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    tab.classList.add('active');
+    target.classList.add('active');
+  });
 });
